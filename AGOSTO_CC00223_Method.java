faa86e4e74e2a58f25bf24f57e08d7b2f8533328
cagosto250000001184-cyber <cagosto_250000001184@uic.edu.ph>
2025-10-16 17:51:04 +0800

    Add files via upload

---
+++ AGOSTO_CC00223_Method.java
@@ -0,0 +1,19 @@
+public class AGOSTO_CC00223_Method {
+    public static void TOP1 () {
+        System.out.println("TOP 1 : AGOSTO");
+    }
+
+    public static void TOP2() {
+        System.out.println("TOP 2 : LAKSAANE ");
+    }
+
+    public static void TOP3() {
+        System.out.println("TOP 3: CRO");
+    }
+
+    public static void main(String[] args) {
+        TOP1();
+        TOP2();
+        TOP3();
+    }
+}
