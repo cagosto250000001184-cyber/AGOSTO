8fb64596d209c178c8d79462dbbce52b744242df
cagosto250000001184-cyber <cagosto_250000001184@uic.edu.ph>
2025-10-16 17:23:43 +0800

    Add files via upload

---
+++ Agosto_CC00223_Methods.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package agosto_cc00223_methods;
+
+/**
+ *
+ * @author LAKSANE
+ */
+public class AGOSTO_CC00223_Methods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
